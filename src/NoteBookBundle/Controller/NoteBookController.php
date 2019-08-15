6dbb6ec7647ccf5653fb5cf92f4d2801f3a3a961
ewasos <ploewa@hotmail.com>
2019-08-15 14:08:54 +0200

    identity of the project: css+bundle

--- src/NoteBookBundle/Controller/NoteBookController.php
+++ src/NoteBookBundle/Controller/NoteBookController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace AppBundle\Controller;
+namespace NoteBookBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -14,7 +14,7 @@ class NoteBookController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
+        return $this->render('notebook/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
